f0eb845e1aa0d6a34db806d6f9f1e3ac52f065fa
kadir-zeren <k4dzeren@gmail.com>
2023-11-20 13:57:04 +0300

    v3

--- app.js
+++ app.js
@@ -15,3 +15,24 @@ const euro = "2000";
 const totalMoney = Number(dolar) + +euro;
 
 console.log(totalMoney);
+
+const euroInt = parseInt(euro);
+console.log(euroInt);
+const dolarFloat = parseFloat(dolar);
+console.log(dolarFloat);
+
+console.log(parseFloat(null));
+console.log(parseFloat(""));
+
+const myNum = 2000.5;
+console.log(String(myNum));
+
+const calismaSuresi = prompt("Calisma Suresi");
+let maas = prompt("Maasini giriniz");
+
+if (calismaSuresi >= 10) {
+  maas = maas * 1.1;
+  console.log("Zamli maasiniz:", maas);
+} else {
+  console.log("Uzgunuz maasiniza zam yapamiyoruz", maas);
+}
