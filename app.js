16a74b59033343bb66b1fcdc63e53ee5ec36d4ea
kadir-zeren <k4dzeren@gmail.com>
2023-11-20 19:41:51 +0300

    v6

--- app.js
+++ app.js
@@ -44,57 +44,86 @@
 // }
 // console.log("bye bye");
 
-const grade = prompt("please enter your grade:");
-let result = null;
-if (grade < 0) {
-  console.log("Grade can not be small than zero");
-} else if (grade < 40) {
-  result = "FF";
-} else if (grade <= 50) {
-  result = "DD";
-} else if (grade <= 65) {
-  result = "CC";
-} else if (grade <= 89) {
-  result = "BB";
-} else if (grade <= 100) {
-  result = "AA";
-} else {
-  console.log("Grade can not be bigger than 100");
-}
+// const grade = prompt("please enter your grade:");
+// let result = null;
+// if (grade < 0) {
+//   console.log("Grade can not be small than zero");
+// } else if (grade < 40) {
+//   result = "FF";
+// } else if (grade <= 50) {
+//   result = "DD";
+// } else if (grade <= 65) {
+//   result = "CC";
+// } else if (grade <= 89) {
+//   result = "BB";
+// } else if (grade <= 100) {
+//   result = "AA";
+// } else {
+//   console.log("Grade can not be bigger than 100");
+// }
 
-if (result != null) {
-  console.log(`Your score:${result}`);
-}
+// if (result != null) {
+//   console.log(`Your score:${result}`);
+// }
 
-const n1 = +prompt("Num1:");
-const n2 = +prompt("Num2:");
-const n3 = +prompt("Num3:");
+// const n1 = +prompt("Num1:");
+// const n2 = +prompt("Num2:");
+// const n3 = +prompt("Num3:");
 
-if (n1 > n2) {
-  if (n1 > n3) {
-    console.log("Biggest:", n1);
-  }
-}
+// if (n1 > n2) {
+//   if (n1 > n3) {
+//     console.log("Biggest:", n1);
+//   }
+// }
 
-if (n1 >= n2 && n1 >= n3) {
-  console.log("Biggest:", n1);
-} else if (n2 >= n1 && n2 >= n3) {
-  console.log("Biggest:", n2);
-} else if (n3 >= n1 && n3 >= n2) {
-  console.log("Biggest:", n3);
-}
+// if (n1 >= n2 && n1 >= n3) {
+//   console.log("Biggest:", n1);
+// } else if (n2 >= n1 && n2 >= n3) {
+//   console.log("Biggest:", n2);
+// } else if (n3 >= n1 && n3 >= n2) {
+//   console.log("Biggest:", n3);
+// }
 
-let biggest = n1;
-if (n2 >= biggest) {
-  biggest = n2;
-}
-if (n3 >= biggest) {
-  biggest = n3;
-}
-console.log(`${biggest} is biggest`);
+// let biggest = n1;
+// if (n2 >= biggest) {
+//   biggest = n2;
+// }
+// if (n3 >= biggest) {
+//   biggest = n3;
+// }
+// console.log(`${biggest} is biggest`);
 
 const grade1 = prompt("please enter your grade:");
 
 grade1 >= 50 ? console.log("SUCCEED") : console.log("FAILED");
 const score = grade1 >= 50 ? "PASSED" : "FAILED";
 console.log(score);
+
+const score1 = grade1 >= 50 ? "SUCCEED" : "";
+console.log(score1);
+
+const speed = 110;
+speed > 120
+  ? console.log("speedy")
+  : speed >= 90
+  ? console.log("Normal")
+  : console.log("Low Speed");
+
+if (speed > 120) {
+  console.log("speedy");
+} else if (speed >= 90) {
+  console.log("Normal");
+} else {
+  console.log("Low Speed");
+}
+
+let description = "";
+const age = 19;
+if (age >= 18) {
+  description = "Adult";
+}
+
+age >= 18 && (description = "Adult");
+console.log(description);
+
+age >= 18 || (description = "young");
